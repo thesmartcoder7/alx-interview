0c13591776fb0a270842fc59998ab447bab75889
samuel martins <samuel.martins4.sm@gmail.com>
2023-12-01 02:48:45 +0300

    chore: implement the pascal's triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""
+A function that returns a list of lists of integers
+"""
+
+
+def pascal_triangle(n):
+    """
+    Returns a list of lists of integers representing the Pascal’s triangle of n
+    """
+    if n <= 0:
+        return []
+    if n == 1:
+        return [[1]]
+    if n == 2:
+        return [[1], [1, 1]]
+    tri = [[1], [1, 1]]
+    for i in range(2, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(tri[i - 1][j - 1] + tri[i - 1][j])
+        row.append(1)
+        tri.append(row)
+    return tri
---
+++ 0x00-pascal_triangle/README.md
@@ -0,0 +1,26 @@
+# Pascal's Triangle
+
+Create a function def pascal_triangle(n): that returns a list of lists of integers representing the Pascal’s triangle of n:
+
+Test Script:
+
+```python
+
+#!/usr/bin/python3
+"""
+0-main
+"""
+pascal_triangle = __import__('0-pascal_triangle').pascal_triangle
+
+def print_triangle(triangle):
+    """
+    Print the triangle
+    """
+    for row in triangle:
+        print("[{}]".format(",".join([str(x) for x in row])))
+
+
+if __name__ == "__main__":
+    print_triangle(pascal_triangle(5))
+
+```
